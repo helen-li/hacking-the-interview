00d12d5c62c5d7a098dcca193f417010abf6d411
Aidan <aidan.li@mail.utoronto.ca>
2022-09-18 00:43:14 -0400

    added record audio skeleton? L reacy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,22 @@ import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/
 import { Breadcrumb, Layout, Menu, Typography, Card, Button, Space, Progress } from 'antd';
 import React from 'react';
 import "antd/dist/antd.css";
+import recordAudio from './recordAudio';
 const { Header, Content, Footer, Sider } = Layout;
 
 const { Title } = Typography;
 
 const questions = ["Tell me about yourself.",
-"Why do you want to work at this company?",
-,"Why do you want this job?"
-,"Why should we hire you?"
-,"What can you bring to the company?"
-,"What are your greatest strengths?"
-,"What do you consider to be your weaknesses?"
-,"What is your greatest professional achievement?"
-,"Tell me about a challenge or conflict you’ve faced at work, and how you dealt with it."
-,"Tell me about a time you demonstrated leadership skills."
-,"What’s a time you disagreed with a decision that was made at work?"];
+  "Why do you want to work at this company?",
+  , "Why do you want this job?"
+  , "Why should we hire you?"
+  , "What can you bring to the company?"
+  , "What are your greatest strengths?"
+  , "What do you consider to be your weaknesses?"
+  , "What is your greatest professional achievement?"
+  , "Tell me about a challenge or conflict you’ve faced at work, and how you dealt with it."
+  , "Tell me about a time you demonstrated leadership skills."
+  , "What’s a time you disagreed with a decision that was made at work?"];
 
 const items2 = questions.map((question, index) => {
   const key = String(index + 1);
@@ -55,7 +56,7 @@ const App = () => (
           padding: '24px 0',
         }}
       >
-       
+
         <Sider width={400}>
           <Menu
             mode="inline"
@@ -63,7 +64,7 @@ const App = () => (
             defaultOpenKeys={['sub1']}
             style={{
               height: '100%',
-               width: 400,
+              width: 400,
             }}
             items={items2}
           />
@@ -75,50 +76,50 @@ const App = () => (
             minHeight: 280,
           }}
         >
-           <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+          <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
             <Card title="Tell me about yourself."
-    style={{ width: 900 }}
-    actions={[
-      <Button>Start here!</Button>,
-    ]}
-  >
-    <p>Are you ready to talk about yourself during a job interview? Interviewers will sometimes start an interview with an open-ended question like, "Tell me about yourself."
+              style={{ width: 900 }}
+              actions={[
+                <Button onClick="recordAudio()">Start here!</Button>,
+              ]}
+            >
+              <p>Are you ready to talk about yourself during a job interview? Interviewers will sometimes start an interview with an open-ended question like, "Tell me about yourself."
 
-This question is a way to break the ice and make you feel more comfortable during the interview process. However, some people might find this—and other interview questions about you—slightly stressful.
+                This question is a way to break the ice and make you feel more comfortable during the interview process. However, some people might find this—and other interview questions about you—slightly stressful.
 
-If you’re someone who doesn’t like bragging about yourself, these kinds of questions can be difficult to answer. But they’re a good way for the hiring manager to get insight into your personality, so it pays to prepare to answer them. </p>
-  </Card>
+                If you’re someone who doesn’t like bragging about yourself, these kinds of questions can be difficult to answer. But they’re a good way for the hiring manager to get insight into your personality, so it pays to prepare to answer them. </p>
+            </Card>
 
-  <Card title="Results"
-    style={{ width: 900 }}
-    actions={[
-      <Button>Start here!</Button>,
-    ]}
-  >
-    <Space direction="vertical" size="large" style={{ display: 'flex' }}>
-        <Space direction="horizontal" size="large" style={{ display: 'flex' }}>
-          <Progress type="dashboard" percent={75} />
-            <div style={{ width: 600 }}>
-              Volume
-              <Progress percent={30}/>
-              Stutter
-              <Progress percent={50}/>
-              Repetitiveness
-              <Progress percent={70}/>
-              Enthusiasm
-              <Progress percent={80} />
-            </div>
-        </Space>
+            <Card title="Results"
+              style={{ width: 900 }}
+              actions={[
+                <Button>Start here!</Button>,
+              ]}
+            >
+              <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+                <Space direction="horizontal" size="large" style={{ display: 'flex' }}>
+                  <Progress type="dashboard" percent={75} />
+                  <div style={{ width: 600 }}>
+                    Volume
+                    <Progress percent={30} />
+                    Stutter
+                    <Progress percent={50} />
+                    Repetitiveness
+                    <Progress percent={70} />
+                    Enthusiasm
+                    <Progress percent={80} />
+                  </div>
+                </Space>
 
-        <Title level={4}>You could work on</Title>
+                <Title level={4}>You could work on</Title>
 
-        <Space direction="horizontal" size="large" style={{ display: 'flex' }}>
+                <Space direction="horizontal" size="large" style={{ display: 'flex' }}>
 
-        </Space>
-     </Space>
-   </Card>
-   </Space>
-         
+                </Space>
+              </Space>
+            </Card>
+          </Space>
+
         </Content>
       </Layout>
     </Content>
---
+++ frontend/src/recordAudio.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function recordAudio() {
+    const record = document.querySelector('.record');
+    const stop = document.querySelector('.stop');
+    const soundClips = document.querySelector('.sound-clips');
+    const mainSection = document.querySelector('.main-controls');
+
+    // disable stop button while not recording
+
+    stop.disabled = true;
+
+    //main block for doing the audio recording
+
+    if (navigator.mediaDevices.getUserMedia) {
+        console.log('getUserMedia supported.');
+
+        const constraints = { audio: true };
+        let chunks = [];
+
+        let onSuccess = function (stream) {
+            const mediaRecorder = new MediaRecorder(stream);
+
+            record.onclick = function () {
+                mediaRecorder.start();
+
+                stop.disabled = false;
+                record.disabled = true;
+            }
+
+            stop.onclick = function () {
+                mediaRecorder.stop();
+                mediaRecorder.requestData();
+
+                stop.disabled = true;
+                record.disabled = false;
+            }
+
+            mediaRecorder.onstop = function (e) {
+                const clipName = prompt('Enter a name for your audio file:', 'newaudio');
+                const audio = document.createElement('audio');
+                audio.setAttribute('controls', '');
+
+                audio.controls = true;
+                const blob = new Blob(chunks, { 'type': 'audio/wav; codecs=opus' });
+                chunks = [];
+                const audioURL = window.URL.createObjectURL(blob);
+                audio.src = audioURL;
+                audio.download = 'test.wav';
+            }
+
+            mediaRecorder.ondataavailable = function (e) {
+                chunks.push(e.data);
+            }
+        }
+
+        let onError = function (err) {
+            console.log('The following error occured: ' + err);
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+        return (
+            mediaRecorder.start()
+        );
+
+    } else {
+        console.log('getUserMedia not supported on your browser!');
+        return;
+    }
+}
